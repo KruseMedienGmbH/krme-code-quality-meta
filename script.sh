c49bdbfa87ec768bc9a76a97a9c60a808c35de50
Thomas Buning <tb@krusemedien.com>
2025-01-07 12:41:26 +0100

    chore: update version 6.6

--- script.sh
+++ script.sh
@@ -5,6 +5,9 @@ composer config allow-plugins.symfony/flex true
 composer config allow-plugins.symfony/runtime true
 composer config allow-plugins.phpstan/extension-installer false
 composer config allow-plugins.bamarni/composer-bin-plugin true
+composer config extra.bamarni-bin.bin-links false
+composer config extra.bamarni-bin.target-directory "vendor-bin"
+composer config extra.bamarni-bin.forward-command true
 composer config repositories.krme-code-quality-meta git https://github.com/KruseMedienGmbH/krme-code-quality-meta.git
 
 # Installiert Symfony Flex in Version 2
@@ -14,9 +17,11 @@ composer require --dev symfony/flex "~2" --no-scripts --no-plugins
 composer config extra.symfony.endpoint https://raw.githubusercontent.com/KruseMedienGmbH/krme-recipes/flex/main/index.json
 
 composer config extra.symfony.allow-contrib false
+composer config extra.symfony.docker false
 
 # Installiert das Paket "krme/code-quality-meta"
 composer require --dev krme/code-quality-meta:6.6 --no-scripts --no-plugins
+composer require --dev bamarni/composer-bin-plugin
 
 # Aktualisiert die Symfony Flex-Rezepte
 composer recipes:install krme/code-quality-meta --force
